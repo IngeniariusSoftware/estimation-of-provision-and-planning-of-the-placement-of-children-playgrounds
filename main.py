a1bf72cb3ef27b817ccaab5a39b3a1fc4a18e7b6
Maksim <kyngfy-panda@yandex.ru>
2023-01-03 09:50:19 +0500

    try to use pandana

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import scipy
+import pandana
 import logging
 import osmnx as ox
 import pandas as pd
@@ -7,15 +8,17 @@ import networkx as nx
 import geopandas as gpd
 
 from pathlib import Path
+from pandas import Series
+from pandana import Network
 from shapely import affinity
-from geopandas import GeoDataFrame
-from shapely.geometry import Point, Polygon, LineString
 from networkx import MultiDiGraph
+from geopandas import GeoDataFrame, GeoSeries
+from shapely.geometry import Point, Polygon, LineString
 
 standard_crs = 4326
 block_id_label = 'block_id'
-# equal_area_meter_crs = 6933
-# equal_ratio_meter_crs = 3857
+node_id_label = 'node_id'
+node_distance_label = 'node_distance'
 
 
 def remove_objects_outside_blocks_and_set_block_id(blocks: GeoDataFrame, objects: GeoDataFrame) -> GeoDataFrame:
@@ -29,10 +32,6 @@ def remove_objects_outside_blocks_and_set_block_id(blocks: GeoDataFrame, objects
     inside_objects = inside_objects.astype(dtype={block_id_label: int}, copy=False)
     logging.info(f' {len(objects) - len(inside_objects)} objects outside the blocks were deleted')
     return inside_objects
-
-#
-# def get_square_meters_area(objects: GeoDataFrame) -> list[float]:
-#     return round(objects.geometry.area, 2)
 
 
 def get_geometry_angle(geometry: Polygon):
@@ -73,9 +72,11 @@ def remove_contained_points_in_objects_from_geodataframe(data: GeoDataFrame, blo
     return data.drop(indexes, axis=0)
 
 
-def get_walk_graph(blocks: GeoDataFrame, to_crs=None) -> MultiDiGraph:
-    graph = ox.graph_from_polygon(polygon=blocks.geometry.unary_union.convex_hull, network_type='walk', simplify=True)
-    return ox.project_graph(graph, to_crs=to_crs)
+def get_walk_graph(blocks: GeoDataFrame, to_crs=None) -> Network:
+    graph = ox.graph_from_polygon(polygon=blocks.to_crs(crs=standard_crs).geometry.unary_union.convex_hull, network_type='walk', simplify=True)
+    nodes, edges = ox.graph_to_gdfs(G=ox.project_graph(graph, to_crs=to_crs), nodes=True, edges=True)
+    edges = edges.reset_index()
+    return Network(nodes['x'], nodes['y'], edges['u'], edges['v'], edges[['length']])
 
 
 def get_utm_crs(geometry) -> str:
@@ -84,11 +85,54 @@ def get_utm_crs(geometry) -> str:
     return f'+proj=utm +zone={utm_zone} +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
 
 
-def get_walk_isochrone_polygon(start_point: Point, meters: int, walk_graph: MultiDiGraph) -> Polygon:
-    isochrone_graph = nx.ego_graph(G=walk_graph, n=ox.nearest_nodes(G=walk_graph, X=start_point.x, Y=start_point.y), radius=meters, distance='weight')
-    node_points = [Point((data['x'], data['y'])) for node, data in isochrone_graph.nodes(data=True)]
-    return gpd.GeoSeries(node_points).unary_union.convex_hull
+def get_isochrone_walk_graph(start_point: Point, meters: int, walk_graph: MultiDiGraph) -> MultiDiGraph:
+    return nx.ego_graph(G=walk_graph, n=ox.nearest_nodes(G=walk_graph, X=start_point.x, Y=start_point.y), radius=meters, distance='length')
 
+
+def graph_to_polygon(graph: MultiDiGraph) -> Polygon:
+    node_points = [Point((data['x'], data['y'])) for node, data in graph.nodes(data=True)]
+    return GeoSeries(node_points).unary_union.convex_hull
+
+
+def polygon_to_x_y_coords(polygon: Polygon) -> [float, float]:
+    centroid = polygon.centroid
+    return centroid.x, centroid.y
+
+
+def geodataframe_to_x_y_coords(objects: GeoDataFrame) -> [Series, Series]:
+    x_coords, y_coords = zip(*objects.geometry.apply(func=lambda x: polygon_to_x_y_coords(x)))
+    return Series(x_coords), Series(y_coords)
+
+
+def get_gravity_for_building_and_playground(area_playground: float, distance: float) -> float:
+    return area_playground / (distance + 50.0) ** 2.0
+
+
+def set_nearest_nodes_to_objects(objects: GeoDataFrame, walk_graph: Network) -> None:
+    x_coords, y_coords = geodataframe_to_x_y_coords(objects=objects)
+    objects[node_id_label] = walk_graph.get_node_ids(x_col=x_coords, y_col=y_coords)
+    # objects[node_distance_label] =
+
+
+def distribute_people_to_playgrounds(blocks: GeoDataFrame, living_buildings: GeoDataFrame, playgrounds: GeoDataFrame, walk_graph: Network) -> None:
+    set_nearest_nodes_to_objects(playgrounds, walk_graph)
+    set_nearest_nodes_to_objects(living_buildings, walk_graph)
+    max_meters_to_playground = 500
+
+    print(walk_graph.nodes_in_range([living_buildings.iloc[0][node_id_label]], radius=500))
+
+    # for _, block in blocks.iterrows():
+    #     block_living_buildings = living_buildings[living_buildings[block_id_label] == block['id']]
+    #
+    #     walk_graph.nodes_in_range()
+    #     for _, living_building in block_living_buildings.iterrows():
+    #
+    #         graph = get_walk_isochrone_graph(start_point=living_building.geometry.centroid, meters=max_meters_to_playground, walk_graph=walk_graph)
+    #         isochrone = graph_to_polygon(graph)
+    #         available_playgrounds = playgrounds[playgrounds.intersects(other=isochrone)]
+    #         for _, playground in available_playgrounds.iterrows():
+    #
+    #         # playgrounds_gravity = available_playgrounds.apply(func=lambda x: get_gravity_for_building_and_playground(x))
 
 def main():
     test_folder = Path() / 'data' / 'test'
@@ -109,17 +153,32 @@ def main():
     playgrounds = remove_objects_outside_blocks_and_set_block_id(blocks=blocks, objects=playgrounds)
     playgrounds = remove_contained_points_in_objects_from_geodataframe(data=playgrounds, blocks=blocks, objects=buildings)
     playgrounds['area'] = round(playgrounds.geometry.area, 2)
+    playgrounds['population'] = [0] * len(playgrounds)
     playgrounds = transform_point_objects_into_square_polygons_with_median_area(blocks=blocks, objects=playgrounds)
 
-    walk_graph_file_path = test_folder / 'walk_graph.graphml'
-    if walk_graph_file_path.exists():
-        walk_graph = ox.load_graphml(filepath=walk_graph_file_path)
+    nodes_file_path, edges_file_path = test_folder / 'nodes.csv', test_folder / 'edges.csv'
+    if nodes_file_path.exists() and edges_file_path.exists():
+        nodes, edges = pd.read_csv(nodes_file_path, index_col=0), pd.read_csv(edges_file_path, index_col=0)
+        walk_graph = pandana.Network(nodes['x'], nodes['y'], edges['from'], edges['to'], GeoDataFrame({'distance': edges['length']}))
     else:
         walk_graph = get_walk_graph(blocks=blocks, to_crs=utm_crs)
-        ox.save_graphml(G=walk_graph, filepath=walk_graph_file_path)
+        walk_graph.nodes_df.to_csv(nodes_file_path), walk_graph.edges_df.to_csv(edges_file_path)
 
-    poly = get_walk_isochrone_polygon(start_point=living_buildings.iloc[0].geometry.centroid, meters=500, walk_graph=walk_graph)
-    gpd.GeoDataFrame(geometry=poly).set_crs(crs=utm_crs).to_crs(crs=standard_crs).to_file(filepath='isopoly.geojson', drive='GeoJSON')
+    distribute_people_to_playgrounds(blocks, living_buildings, playgrounds, walk_graph)
 
+    # walk_graph.get_node_ids(x_col=playgrounds, y_col=)
+    # graph = get_isochrone_walk_graph(start_point=living_buildings.iloc[0].geometry.centroid, meters=500, walk_graph=walk_graph)
+    # polygon = graph_to_polygon(graph=graph)
+    # available_playgrounds = playgrounds[playgrounds.intersects(other=polygon)]
+    # for _, playground in available_playgrounds.iterrows():
+    #     start_point = living_buildings.iloc[0].geometry.centroid
+    #     start_node = ox.nearest_nodes(G=walk_graph, X=start_point.x, Y=start_point.y)
+    #
+    #     end_point = playground.geometry.centroid
+    #     end_node = ox.nearest_nodes(G=walk_graph, X=end_point.x, Y=end_point.y)
+    #
+    #     distance = ox.shortest_path(G=walk_graph, orig=start_node, dest=end_node, weight='length')
+    #     print(distance)
+    
 
 main()
